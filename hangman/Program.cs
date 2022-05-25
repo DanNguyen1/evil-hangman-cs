2401587682a75749902e9c8e64387e7e330c03b3
Dan Nguyen <dnguyen12302@gmail.com>
2022-05-25 09:11:46 -0400

    dan: set up file structure, created basic Hello World program

---
+++ hangman/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+namespace myProgram
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
